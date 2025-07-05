380a658a972eceeb6ed32ace697ae7ce0d4835f3
felipe2458 <luizfelipealvesdematos10@gmail.com>
2025-07-05 15:08:18 -0300

    switch

--- main.cpp
+++ main.cpp
@@ -3,18 +3,79 @@
 int main(){
     using namespace std;
 
-    int age;
+    /*int month;
 
-    cout << "What´s your age?: ";
-    cin >> age;
+    cout << "Enter your month (1-12): ";
+    cin >> month;
 
-    if(age >= 18){
-        cout << "Welcome to site!";
-    }else if(age < 0){
-        cout << "You heven´t been born yet!";
-    }else{
-        cout << "Sorry, you`re too young!";
+    switch(month){
+        case 1:
+            cout << "It is January!";
+            break;
+        case 2:
+            cout << "It is February!";
+            break;
+        case 3:
+            cout << "It is March!";
+            break;
+        case 4:
+            cout << "It is April!";
+            break;
+        case 5:
+            cout << "It is May!";
+            break;
+        case 6:
+            cout << "It is June!";
+            break;
+        case 7:
+            cout << "It is July!";
+            break;
+        case 8:
+            cout << "It is August!";
+            break;
+        case 9:
+            cout << "It is September!";
+            break;
+        case 10:
+            cout << "It is October!";
+            break;
+        case 11:
+            cout << "It is November!";
+            break;
+        case 12:
+            cout << "It is December!";
+            break;
+        default:
+            cout << "Please enter in only numbers between 1 and 12!";
+            break;
+    }*/
+
+    char grade;
+
+    cout << "What´s letter grade?: ";
+    cin >> grade;
+
+    switch(grade){
+        case 'A':
+            cout << "Excellent!";
+            break;
+        case 'B':
+            cout << "Good!";
+            break;
+        case 'C':
+            cout << "Average!";
+            break;
+        case 'D':
+            cout << "Poor!";
+            break;
+        case 'F':
+            cout << "Fail!";
+            break;
+        default:
+            cout << "Please enter in only letters A, B, C, D, F!";
+            break;
     }
+    
 
     return 0;
 }
