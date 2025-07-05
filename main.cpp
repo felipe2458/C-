5d82991d82d1d879328d96f593a28c376b08633f
felipe2458 <luizfelipealvesdematos10@gmail.com>
2025-07-05 13:03:07 -0300

    Namespaces

--- main.cpp
+++ main.cpp
@@ -1,10 +1,23 @@
 #include <iostream>
 
+namespace first{
+    int x = 10 + 23;
+}
+
+namespace second{
+    int x = 100 + 323;
+}
+
 int main(){
-    const double PI = 3.14159;
-    const int LIGTH_SPEED = 10;
-    const int WIDTH = 1920;
-    const int HEIGHT = 1080;
+    using namespace std;
+    using namespace first;
+
+    //* int x = 0; caso tenha o: using namespace first e o: int x = 0; quando for usar a variavel x, sera usado o que está no escopo do int main(){}
+
+    // std::cout << x;
+    // para usar isso: std::cout << first::x; não é preciso usar o: using namespace first;
+
+    cout << x;
 
     return 0;
 }
