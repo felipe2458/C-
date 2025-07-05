fb4f265b30131199ac6487c8b48422f87835618d
felipe2458 <luizfelipealvesdematos10@gmail.com>
2025-07-05 12:35:07 -0300

    Variaveis

--- main.cpp
+++ main.cpp
@@ -1,8 +1,52 @@
 #include <iostream>
 
 int main(){
-    std::cout << "Hello, World!" << '\n';
-    std::cout << "My name is: Felipe" << '\n';
+    //* whole numbers (int)
+
+    int x = 5;
+    int y = 10;
+    int sum = x + y;
+
+    // std::cout << x << '\n';
+    // std::cout << y << '\n';
+    // std::cout << sum << '\n';
+    
+
+    int age = 15;
+    int year = 2025;
+    int days = 7;
+
+    // std::cout << days << '\n';
+
+    //* Number including decimal (double)
+
+    double price = 10.99;
+    double gpa = 2.5;
+    double temperature = 98.6;
+
+    // std::cout << price << '\n';
+
+    //* Single character (char)
+    char initial = 'M';
+    char initial2 = 'J';
+    char currency = '$';
+
+    // std::cout << initial << initial2 << '\n';
+
+    //* Boolean (bool = true or false)
+    
+    bool isStudent = false;
+    bool power = true;
+    bool forSale = true;
+
+    //* String (std::string)
+
+    std::string name = "Felipe";
+    std::string day = "Friday";
+    std::string food = "Pizza";
+
+    std::cout << "Hello, " << name << "!\n";
+    std::cout << "You are " << age << " years old.\n";
 
     return 0;
 }
